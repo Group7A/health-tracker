4e6a7ef2e4d40fdbc5b6e455c5193346dc00661f
Ming Yang <mingyang2.71@gmail.com>
2017-11-14 16:41:00 -0500

    sorting done and provide different alternatives

--- usda/usda.js
+++ usda/usda.js
@@ -21,6 +21,10 @@ function($scope, $http) {
 	$scope.have_match = 0;
 	$scope.c_method = "baked";
 	$scope.alt_food_object = {};
+	$scope.top_alt_count = 3;
+	//0 - want single healthies alt, 1 - want single "tastiest" alt, 2, want 3 alts for ingredients
+	$scope.alt_reqest = 0;
+	$scope.mid_ind;
 
 	/*
 	$scope.sortAltFood = () => {
@@ -50,39 +54,51 @@ function($scope, $http) {
 	}
 	*/
 
-	$http.get('food_alternatives.json')
-	.then ((response) =>{
-		$scope.alt_food_object = response.data;
-		$scope.alt_food_object.cooking_methods.forEach((cooking_method, i) => {
-			cooking_method.food_groups.forEach( (food_group, j) => {
-				food_group.food_alts.sort(function(a, b) {
-					var nut_valA = a.db_main_nutrient.db_amount;
-					var nut_valB = b.db_main_nutrient.db_amount;
-					if(nut_valA < nut_valB){
-						return -1;
-					}
-					else if(nut_valA > nut_valB){
-						return 1;
-					}	
-					else{
-						return 0;
-					}
+	function sort_alt() {
+		$http.get('food_alternatives.json')
+		.then ((response) =>{
+			$scope.alt_food_object = response.data;
+			setTimeout(console.log($scope.alt_food_object), 1000);
+			$scope.alt_food_object.cooking_methods.forEach((cooking_method, i) => {
+				cooking_method.food_groups.forEach( (food_group, j) => {
+					food_group.food_alts.sort(function(a, b) {
+						var nut_valA = a.db_main_nutrient.db_amount;
+						var nut_valB = b.db_main_nutrient.db_amount;
+						if(nut_valA < nut_valB){
+							return 1;
+						}
+						else if(nut_valA > nut_valB){
+							return -1;
+						}	
+						else{
+							return 0;
+						}
+					});
 				});
 			});
 		});
-	});
-	console.log($scope.alt_food_object);
-	console.log('hu');
+	}
 
+	sort_alt();
 
-
+	/*
 	$scope.getAltFood = () => {
 		$http.get('food_alternatives.json')
 			.then( (response) => {
 				response.data.cooking_methods.forEach( (cooking_method, i) => {
 					cooking_method.food_groups.forEach( (food_group, j) => {
 						food_group.food_alts.forEach( (food_alt, k) => {
+							
+							if((food_alt.db_name == $scope.search) && ($scope.c_method == cooking_method.method_name)){
+								$scope.have_match = 1;
+								$scope.orig_ndbno = food_alt.db_ndbno;
+							}
+							else if ((food_alt.db_name != $scope.search) && ($scope.have_match == 1) && ($scope.top_alt_count > 0)){
+								$scope.map.push(food_alt);
+								$scope.top_alt_count = $scope.top_alt_count-1;
+							}
 
+							/*
 							if((food_alt.db_name == $scope.search) && ($scope.c_method == cooking_method.method_name)){
 								$scope.have_match = 1;
 								$scope.orig_ndbno = food_alt.db_ndbno;
@@ -92,6 +108,8 @@ function($scope, $http) {
 							else{
 								$scope.all_alt_in_group.push(food_alt);
 							}
+							
+
 						});
 						if($scope.have_match == 1){
 							$scope.all_alt_in_group.forEach((alt_item, i) => {
@@ -108,8 +126,63 @@ function($scope, $http) {
 			});
 		$scope.orig_nutrient_amount = 0;
 		$scope.map = [];
+		$scope.top_alt_count = 3;
 	}
 	//end of getting alt food
+	*/
+
+	$scope.getAltFood = () => {
+		console.log('hi');
+		$scope.alt_food_object.cooking_methods.forEach( (cooking_method, i) => {
+			cooking_method.food_groups.forEach( (food_group, j) => {
+				food_group.food_alts.forEach( (food_alt, k) => {
+					
+					if((food_alt.db_name == $scope.search) && ($scope.c_method == cooking_method.method_name)){
+						$scope.have_match = 1;
+					}
+					else if ((food_alt.db_name != $scope.search) && ($scope.have_match == 1)){
+						$scope.all_alt_in_group.push(food_alt);
+					}			
+				});
+				$scope.have_match = 0;
+			});
+		});
+
+		if($scope.alt_reqest == 0){
+			var alt_item = $scope.all_alt_in_group[$scope.all_alt_in_group.length-1];
+			$scope.map.push({"map_ndbno": alt_item.db_ndbno, "map_name": alt_item.db_name, "nutrient": alt_item.db_main_nutrient.db_amount});
+		}
+		else if($scope.alt_request == 1){
+			var alt_item = $scope.all_alt_in_group[0];
+			$scope.map.push({"map_ndbno": alt_item.db_ndbno, "map_name": alt_item.db_name, "nutrient": alt_item.db_main_nutrient.db_amount});
+		}
+		else if($scope.alt_request == 2){
+			if($scope.all_alt_in_group.length < $scope.top_alt_count){
+				$scope.all_alt_in_group.forEach((alt_item, i) => {
+					$scope.map.push({"map_ndbno": alt_item.db_ndbno, "map_name": alt_item.db_name, "nutrient": alt_item.db_main_nutrient.db_amount});
+				});
+			}
+			else{
+				//control what alt we give
+				mid_ind = $scope.all_alt_in_group.length/2;
+				//index is not whole number
+				if(mid_ind % 1 != 0){
+					mid_ind = mid_ind - 0.5;
+				}
+				$scope.all_alt_in_group.forEach((alt_item, i) => {
+					if(i==0 || i==mid_ind || i==$scope.all_alt_in_group.length-1){
+						$scope.map.push({"map_ndbno": alt_item.db_ndbno, "map_name": alt_item.db_name, "nutrient": alt_item.db_main_nutrient.db_amount});
+						console.log($scope.map);
+					}
+				});
+			}
+			
+		}
+		console.log($scope.map);
+		$scope.all_alt_in_group = [];
+		$scope.map = [];
+		$scope.mid_ind = 0;
+	}
 
 	$scope.doSearch = () => {
 		var searchURL = 
