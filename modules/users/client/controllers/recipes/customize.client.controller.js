ed79e94af1f74da6937013c78667b0e721618c29
Zach <zmurta@ufl.edu>
2017-11-26 12:19:51 -0500

    get report function

--- modules/users/client/controllers/recipes/customize.client.controller.js
+++ modules/users/client/controllers/recipes/customize.client.controller.js
@@ -36,6 +36,55 @@
     //console.log($stateParams.multiple_map);
     console.log($scope.multiple_alternatives);
 
+    
+    // API KEY
+    var apiKey = 'YAJ2M9l67OaqNMPCEfBcoccVtQDY5LPUR20rFzP8';
+    
+        // FOR REPORT
+        var type = 'b';
+        var format = 'json';
+    
+        // FOR INDIVIDUAL SEARCHES
+        var sort = 'n';
+        var max = '200';
+        var ds = 'Standard Reference';
+    
+        function getReport (alternative) {
+          var reportURL = 
+              'http://api.nal.usda.gov/ndb/reports/' + 
+              '?ndbno=' + alternative.map_ndbno + 
+              '&type=' + type + 
+              '&format=' + format + 
+              '&api_key=' + apiKey; 
+    
+          getURL(reportURL)
+              .then((results) => {
+                $scope.searched = results.data;
+                assignFood(alternative);
+              });
+        };
+    
+        function assignFood(alternative) {
+          $scope.food = $scope.searched.report.food.name.toLowerCase();
+          alternative.nutrients = [];
+    
+          $scope.searched.report.food.nutrients.forEach((nutrient, i) => {
+            if (nutrient.name === 'Protein') alternative.nutrients.push(nutrient);
+            else if (nutrient.name === 'Total lipid (fat)') alternative.nutrients.push(nutrient);
+            else if (nutrient.name === 'Carbohydrate, by difference') alternative.nutrients.push(nutrient);
+            else if (nutrient.name === 'Fiber, total dietary') alternative.nutrients.push(nutrient);
+            else if (nutrient.name === 'Sugars, total') alternative.nutrients.push(nutrient);
+            else if (nutrient.name === 'Cholesterol') alternative.nutrients.push(nutrient);
+          });
+    
+          console.log('nutrients: ', alternative.nutrients);
+        }
+    
+        function getURL(url) {
+          return $http.get(url);
+        }
+    
+
 
     // vm.customizings = UsersService.query();
   }
