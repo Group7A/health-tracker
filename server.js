0dcd9e7e7b6dc222332618c59f50bc2896c93c29
Himal <himpatel@ufl.edu>
2017-11-28 06:01:45 -0500

    I have written several tests but only couple of them run

--- server.js
+++ server.js
@@ -5,3 +5,22 @@
  */
 var app = require('./config/lib/app');
 var server = app.start();
+var express = require('express'),
+	bodyParser = require('body-parser'),
+    morgan = require('morgan'),
+    path = require('path');
+
+express.use(bodyParser.urlencoded({ extended: true }));
+express.use(bodyParser.json());
+
+express.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
+
+express.use(morgan('dev'));
+
+express.use(express.static(__dirname + '/modules'));
+express.listen(3000);
